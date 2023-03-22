72faefd3c051d0e1d2e1f5dd9fe7a134d4bb462d
Hidaya-M <doyimegateli@gmail.com>
2023-03-22 22:35:09 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css"
+import React from 'react'
+import axios from "axios"
+
+class App extends React.Component {
+  state = {advice : ''};
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+  fetchAdvice = () => {
+    axios.get("https://api.adviceslip.com/advice")
+    .then( (response)  => {
+      const {advice} = response.data.slip
+      this.setState({advice});
+      console.log(advice);
+    })
+    .catch( (error)  =>  {
+      console.log(error);
+    });
+  }
+  
+  render() {
+    const {advice} = this.state;
+    return(
+  <div className="app">
+    <div className="card">
+     <h1  className="heading">{advice}</h1>
+     <button onClick={this.fetchAdvice}> advice
+     </button>
+    </div>
+  </div>
+    );
+  }
+}
+
+
+export default App
